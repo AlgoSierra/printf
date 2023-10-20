bc7c2201b228b704944f8cc1722a2534b50ab817
AlgoSierra <nangiroevanstioko@gmail.com>
2023-10-20 16:08:31 -0700

    0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

---
+++ _printf.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+ * _printf - function that produces output according to format
+ * formart: format
+ *
+ * Return: number of character printed. Exclude \0
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+
+	int count = 0;
+	const char *ptr = format;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr !+ '%')
+		{
+			ptr++;
+			switch (*ptr)
+			{
+				case 'c':
+					count += putchar(va_args(args, int));
+					break;
+				case 's':
+					count += printf("%s", va_arg(args, char *));
+					break;
+				case '%':
+					count += putchar('%');
+					break;
+				default:
+					putchar('%');
+					count++;
+					break;
+			}
+		}
+		else
+		{
+			putchar(*ptr);
+			count++;
+		}
+		ptr++;
+	}
+	va_end(args);
+
+	return (count);
+}
+
+int main()
+{
+	int num = 42;
+	char *str = "Hello";
+
+	int printed = _printed("This is a number: %d, and this is a string: %s%%\n, num, str");
+	printf("Number of characters printed: %d\n", printed);
+
+	return (0);
+}
